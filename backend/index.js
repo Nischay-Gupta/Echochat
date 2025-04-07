961476697d509876426d32ba2d30cb9849d8b455
nischay-gupta <nischaygupta2004@gmail.com>
2025-04-07 12:48:36 +0530

    add client url

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: process.env.CLIENT_URL, 
+    origin: `${process.env.CLIENT_URL}`, 
     methods: 'GET,POST,PUT,DELETE,PATCH', 
     allowedHeaders: 'Content-Type,Authorization', 
     credentials: true, 
