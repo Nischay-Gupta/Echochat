f1f2af165828bc1d1af54761c9641aed584178a0
nischay-gupta <nischaygupta2004@gmail.com>
2025-04-07 12:36:08 +0530

    minor bug fix in backend

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const server = createServer(app)
 const port=process.env.PORT || 8000
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173"
+        origin: process.env.CLIENT_URL
     }
 })
 
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: process.env.CLIENT_URL, 
     methods: 'GET,POST,PUT,DELETE,PATCH', 
     allowedHeaders: 'Content-Type,Authorization', 
     credentials: true, 
